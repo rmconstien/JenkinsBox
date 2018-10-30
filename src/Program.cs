a2303c99485aa5e8dff62c426d9b4f94bfe35b5b
Rachel Constien <rmconsti@uark.edu>
2018-10-30 17:56:47 -0500

    Add files to repo

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace src {
+    public class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string CreateMessage() {
+            return "Hello World";
+        }
+    }
+}
